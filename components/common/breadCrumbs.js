e7d169482be06f43af48cd288126069729514852
muzammil@spiderworks.in <muzammil@spiderworks.in>
2024-11-01 12:03:33 +0530

    change menu color class

--- components/common/breadCrumbs.js
+++ components/common/breadCrumbs.js
@@ -8,11 +8,11 @@ function BreadCrumbs({ secondCrumb, secondCrumbLink, thirdCrumb }) {
                 <div className="col-lg-12 col-md-12 col-12">
                     <nav aria-label="breadcrumb">
                         <ol className="breadcrumb mt-lg-5 mt-md-5 mt-3 mb-lg-4 mb-md-4 mb-3">
-                            <li className="breadcrumb-item"><Link href="/">Home</Link></li>
+                            <li className="breadcrumb-item"><Link className='bread-crumb-link' href="/">Home</Link></li>
                             <li className="breadcrumb-item active" aria-current="page">
                                 {
                                     thirdCrumb ?
-                                        <Link href={`/${secondCrumbLink}` || '#'}>{secondCrumb} </Link>
+                                        <Link className='bread-crumb-link' href={`/${secondCrumbLink}` || '#'}>{secondCrumb} </Link>
                                         :
                                         secondCrumb
                                 }
