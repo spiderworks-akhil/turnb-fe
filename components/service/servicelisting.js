7021b3a8d57290734782bbaf003b450d3d8b0d65
muzammilSW <muzammil@spiderworks.in>
2024-12-12 09:59:56 +0530

    disabled service deatil page linking

--- components/service/servicelisting.js
+++ components/service/servicelisting.js
@@ -57,7 +57,9 @@ const ServicesSection = ({ data, list }) => {
                                                                 </div>
                                                                 <div className="col-lg-9 col-md-9 col-12 bg-services-box">
                                                                     <div className="pt-lft pt-lg-4 pt-md-4 pt-3 pb-0 text-left clearfix aos-init aos-animate" data-aos="fade-up">
-                                                                        <Link style={{textDecoration:'none'}} href={`/services/${obj?.slug}`}> <h5 className="clr-grn mt-0 mb-2"><span className="ft-wt2">{obj?.title}</span></h5></Link>
+                                                                        {/* <Link style={{ textDecoration: 'none' }} href={`/services/${obj?.slug}`}> */}
+                                                                            <h5 className="clr-grn mt-0 mb-2"><span className="ft-wt2">{obj?.title}</span></h5>
+                                                                        {/* </Link> */}
                                                                         <p className="mb-0">{obj?.short_description}</p>
                                                                     </div>
                                                                 </div>
@@ -66,7 +68,9 @@ const ServicesSection = ({ data, list }) => {
                                                             <div className="row g-0">
                                                                 <div className="col-lg-9 col-md-9 col-12 bg-services-box2 pd-ridser">
                                                                     <div className="pt-lg-4 pt-md-4 pt-5 pb-0 pt-lft text-left clearfix aos-init aos-animate" data-aos="fade-up">
-                                                                        <Link style={{textDecoration:'none'}} href={`/services/${obj?.slug}`}><h5 className="clr-grn mt-0 mb-2"><span className="ft-wt2">{obj?.title}</span></h5></Link>
+                                                                        {/* <Link style={{ textDecoration: 'none' }} href={`/services/${obj?.slug}`}> */}
+                                                                            <h5 className="clr-grn mt-0 mb-2"><span className="ft-wt2">{obj?.title}</span></h5>
+                                                                        {/* </Link> */}
                                                                         <p className="mb-0">{obj?.short_description}</p>
                                                                     </div>
                                                                 </div>
