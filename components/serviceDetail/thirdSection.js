f62c55529df8692b2064311a1dafa66b0d1b15e1
muzammilSW <muzammil@spiderworks.in>
2025-02-27 11:34:03 +0530

    space issue

--- components/serviceDetail/thirdSection.js
+++ components/serviceDetail/thirdSection.js
@@ -8,26 +8,32 @@ function ThirdSection({ data }) {
         <div>
             {
                 (data?.content?.title_2 || data?.content?.description_2) &&
-                <div class="container mt-10">
-                    <div class="bg-partner mb-lg-5 mb-md-5 mb-3 p-5 clearfix">
-                        <div class="about-cont2 mt-lg-5 mt-md-5 mt-3 mb-lg-5 mb-md-5 mb-3 clearfix">
-                            <div class="row justify-content-center">
+                <div className="container mt-10">
+                    <div className="bg-partner mb-3 p-5 clearfix">
+                        <div className="about-cont2 clearfix">
+                            <div className="row justify-content-center">
                                 <div className={`${data?.content?.media_id_2?.file_path ? 'col-lg-6 col-md-6' : 'col-lg-12 col-md-12'}col-lg-12 col-md-6 col-12`}>
-                                    <div class="position-relative" data-aos="fade-down">
-                                        <h2 class="title-ak2 text-dark  text-lf mb-lg-3 mb-md-3 mb-3 mt-lg-0 mt-md-0 mt-4">
-                                            {data?.content?.title_2}
-                                        </h2>
-                                        <div class="boxline-new2 mt-lg-3 mt-md-3 mt-2"></div>
-                                        <div class="text-lf position-relative" data-aos="fade-down">
-                                            <div class="bg-image-sec" dangerouslySetInnerHTML={{ __html: data?.content?.description_2 }} />
+                                    <div className="position-relative" data-aos="fade-down">
+                                        {
+                                            data?.content?.title_2 &&
+                                            <h2 className="title-ak2 text-dark  text-lf mb-lg-3 mb-md-3 mb-3 mt-lg-0 mt-md-0 ">
+                                                {data?.content?.title_2}
+                                            </h2>
+                                        }
+                                        {
+                                            data?.content?.title_2 &&
+                                            <div className="boxline-new2 mt-lg-3 mt-md-3 mt-2"></div>
+                                        }
+                                        <div className="text-lf position-relative" data-aos="fade-down">
+                                            <div className="bg-image-sec" dangerouslySetInnerHTML={{ __html: data?.content?.description_2 }} />
                                         </div>
                                     </div>
                                 </div>
                                 {
                                     data?.content?.media_id_2?.file_path &&
-                                    <div class="col-lg-6 col-md-6 col-12">
+                                    <div className="col-lg-6 col-md-6 col-12">
                                         <div className="text-lf position-relative" data-aos="fade-down">
-                                            {/* <Image width={300} height={279} src={data?.content?.media_id_2?.file_path} alt={data?.content?.media_id_2?.alt_text || 'image'} class="img-responsive imgrit" /> */}
+                                            {/* <Image width={300} height={279} src={data?.content?.media_id_2?.file_path} alt={data?.content?.media_id_2?.alt_text || 'image'} className="img-responsive imgrit" /> */}
                                         </div>
                                     </div>
                                 }
