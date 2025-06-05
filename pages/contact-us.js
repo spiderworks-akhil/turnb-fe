9a504265122e7d7714cb3326afe10ab63c5ef546
muzammilSW <muzammil@spiderworks.in>
2025-06-05 15:07:34 +0530

    career block

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,10 +1,5 @@
 import { Inter } from "next/font/google";
 import Layout from "@/components/common/Layout";
-import LifeBanner from "@/components/life/banner";
-import LifeMiddleSection from "@/components/life/middle";
-import BenefitsSection from "@/components/life/benefits";
-import LifeTestimonials from "@/components/life/testimonial";
-import LifeEventsSection from "@/components/life/events";
 import ContactBanner from "@/components/contact/banner";
 import ContactForm from "@/components/contact/form";
 import ContactMap from "@/components/contact/map";
