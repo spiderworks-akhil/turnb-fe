527db6f516c7f411938cc0233166062a0eefba59
Lijith <lijith@spiderworks.in>
2025-11-19 15:01:15 +0530

    changes

--- components/AIExpertise/features.js
+++ components/AIExpertise/features.js
@@ -2,38 +2,43 @@ import Image from 'next/image'
 import { ArrowRightIcon } from '../common/Button'
 import FeatImg from '../../public/img/ai/feat.png'
 
-const AiFeatures = ({data}) => {
+import back from '../../public/img/gradient/back.png'
+
+const AiFeatures = ({ data }) => {
   return (
     <section className='ai-features-sec'>
       <div className='container '>
         <h3>  {data?.content?.title_4}  </h3>
 
-   {data?.content?.listing_id_future_points?.map((feat, index) => (
-          <div className="feat-list" key={index} style={{ "--i": index + 1 }}>
-            <div className="row">
-              <div className="col-md-5">
-                <Image
-                  width={750}
-                  height={350}
-                  src={feat?.media_id?.file_path}
-                  alt={feat?.alt_text}
-                />
-              </div>
-              <div className="col-md-7">
+        {data?.content?.listing_id_future_points?.map((feat, index) => (
+          <div className="feat-list " key={index} style={{ "--i": index + 1 }}>
+            <div className=' w-full h-full relative'>
+              <Image alt='background gradient'
+                src={back}
+                className='bg-image-gradient w-full h-full object-cover '
+              />
+              <div className="row content-section ">
+                <div className="col-md-5">
+                  <Image
+                    width={750}
+                    height={350}
+                    src={feat?.media_id?.file_path}
+                    alt={feat?.alt_text}
+                  />
+                </div>
                 {/* <span>{String(index + 1).padStart(2, "0")}.</span> */}
-                <h4>{feat?.title}</h4>
-                <div dangerouslySetInnerHTML={{__html:feat?.detailed_description}}/> 
-                
+                <div className="col-md-7 feat-list-content  ">
+                  <h4>{feat?.title}</h4>
+                  <div className='' dangerouslySetInnerHTML={{ __html: feat?.detailed_description }} />
+                </div>
               </div>
             </div>
           </div>
         ))}
 
         {/* Extra Background Feature Block */}
-        <div className="feat-list feat-bg" style={{  top: '-500px' }}></div>
+        <div className="feat-list feat-bg" style={{ top: '-500px' }}></div>
 
- 
-         
       </div>
     </section>
   )
